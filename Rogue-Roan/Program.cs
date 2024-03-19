c35a4c631ac3b9f63a9eae7cc1fa71adc59b975e
RobinPecheur <robin.pecheur@hotmail.fr>
2024-03-19 10:02:39 +0100

    Hello World 👍

---
+++ Rogue-Roan/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Rogue_Roan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
