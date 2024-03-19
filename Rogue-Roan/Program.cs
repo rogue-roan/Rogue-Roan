6bc6042e8ce722a32583f3aec0695f982e3d790f
AntoTilm <antoine.tilman@gmail.com>
2024-03-19 11:26:24 +0100

    a effacer

--- Rogue-Roan/Program.cs
+++ Rogue-Roan/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int roomHeight = 100;
+            int roomWidth = 100;
+
+            int[] roomSize = { 100, 100 };
+
+
+            for (int i = 0; i < roomSize[1]; i++)
+            {
+                Console.Write('-');
+                for (int j = 0; j < roomSize[0]; j++)
+                {
+                    Console.Write('|');
+                }
+            }
+
         }
     }
 }
