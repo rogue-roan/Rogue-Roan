9b7247f0252d644d2177d0a60c7cde4bf5da4aa9
AntoTilm <antoine.tilman@gmail.com>
2024-03-24 17:49:21 +0100

    switch dans le roomRestriction pour choisir aléatoirement le layout de restriction de la piece

--- Rogue-Roan/Models/Mapping/RoomDecoration.cs
+++ Rogue-Roan/Models/Mapping/RoomDecoration.cs
@@ -19,15 +19,11 @@ namespace Rogue_Roan.Models.Mapping
         // A la fin je dois renvoyer un layout de piece
 
 
-
-
-
-        // Récupération de la taille des piece
-        private int roomWidth = 10;
-        private int roomHeight = 10;
+        // A récuperer de Room ou autre?
+        public static int roomWidth = 10;
+        public static int roomHeight = 10;
         //Coefficient par lequel on divise la taille de la piece
         public int factor = 4;
-
 
         public void DecorateRoom(WallAttribute wallAttribute)
         {
@@ -35,115 +31,18 @@ namespace Rogue_Roan.Models.Mapping
             int maxDecoration = (roomHeight * roomWidth) / factor;
 
             // Récupérer les positions des portes:
-            int NorthDoor = 0;
-            int SouthDoor = 0;
-            int WestDoor = 0;
-            int EastDoor = 0;
+            int NorthDoor = (int)WallAttribute.NorthDoor;
+            int SouthDoor = (int)WallAttribute.SouthDoor;
+            int WestDoor = (int)WallAttribute.WestDoor;
+            int EastDoor = (int)WallAttribute.EastDoor;
 
-            // Comment s'assurer qu'il y ai tjs un chemin dans la piece?
-            /*
-             * Soit définir un ou des chemin et empecher la création de décors dessus?
-             * 
-             */
+            var rnd = new Random();
 
+            rnd.Next(1, 2);
+            
 
-            //if (NorthDoor)
-            //{
-
-            //}
-        }
-
-
-
-    
+            
+        }    
     }
 
-    // Il faudrait refactoriser les méthode d'assignation avec des formules mathématiques
-    // Enlever les portes du tableau. Elle me seront fournie
-    public class RoomRestriction
-    {
-        // Chemin de croix
-        public int[][] crossWalk = new int[20][];
-        public void CrossRoadAssignation()
-        {
-
-            // Ligne vertical
-            //crossWalk[0] = new int[] { 0, 5 }; Position d'une porte
-            for (int i = 0; i < Room.Width; i++)
-            {
-
-            }
-            crossWalk[1] = new int[] { 1, 5 };
-            crossWalk[2] = new int[] { 2, 5 };
-            crossWalk[3] = new int[] { 3, 5 };
-            crossWalk[4] = new int[] { 4, 5 };
-            crossWalk[5] = new int[] { 5, 5 };
-            crossWalk[6] = new int[] { 6, 5 };
-            crossWalk[7] = new int[] { 7, 5 };
-            crossWalk[8] = new int[] { 8, 5 };
-            crossWalk[9] = new int[] { 9, 5 };
-            //crossWalk[10] = new int[] { 10, 5 }; Position d'une porte
-
-
-            //Ligne horizontale
-            //crossWalk[0] = new int[] { 5, 0 }; Position d'une porte
-            crossWalk[1] = new int[] { 5, 1 };
-            crossWalk[2] = new int[] { 5, 2 };
-            crossWalk[3] = new int[] { 5, 3 };
-            crossWalk[4] = new int[] { 5, 4 };
-            crossWalk[5] = new int[] { 5, 5 };
-            crossWalk[6] = new int[] { 5, 6 };
-            crossWalk[7] = new int[] { 5, 7 };
-            crossWalk[8] = new int[] { 5, 8 };
-            crossWalk[9] = new int[] { 5, 9 };
-            //crossWalk[10] = new int[] { 5, 10 }; Position d'une porte
-        }
-
-        // Chemin longeant les murs
-
-        public int[][] wallRoad = new int[34][];
-        public void WallRoadAssignation()
-        {
-            // Long du mur nord
-            wallRoad[0] = new int[] { 1, 1 };
-            wallRoad[1] = new int[] { 1, 2 };
-            wallRoad[2] = new int[] { 1, 3 };
-            wallRoad[3] = new int[] { 1, 4 };
-            wallRoad[4] = new int[] { 1, 5 };
-            wallRoad[5] = new int[] { 1, 6 };
-            wallRoad[6] = new int[] { 1, 7 };
-            wallRoad[7] = new int[] { 1, 8 };
-            wallRoad[8] = new int[] { 1, 9 };
-            // Long du mur sud
-            wallRoad[9] = new int[] { 9, 1 };
-            wallRoad[10] = new int[] { 9, 2 };
-            wallRoad[11] = new int[] { 9, 3 };
-            wallRoad[12] = new int[] { 9, 4 };
-            wallRoad[13] = new int[] { 9, 5 };
-            wallRoad[14] = new int[] { 9, 6 };
-            wallRoad[15] = new int[] { 9, 7 };
-            wallRoad[16] = new int[] { 9, 8 };
-            wallRoad[17] = new int[] { 9, 9 };
-            // Long du mur ouest
-            wallRoad[18] = new int[] { 2, 1 };
-            wallRoad[19] = new int[] { 3, 1 };
-            wallRoad[20] = new int[] { 4, 1 };
-            wallRoad[21] = new int[] { 5, 1 };
-            wallRoad[22] = new int[] { 6, 1 };
-            wallRoad[23] = new int[] { 7, 1 };
-            wallRoad[24] = new int[] { 8, 1 };
-            // Long du mur est
-            wallRoad[25] = new int[] { 9, 9 };
-            wallRoad[26] = new int[] { 2, 9 };
-            wallRoad[27] = new int[] { 3, 9 };
-            wallRoad[28] = new int[] { 4, 9 };
-            wallRoad[29] = new int[] { 5, 9 };
-            wallRoad[30] = new int[] { 6, 9 };
-            wallRoad[31] = new int[] { 7, 9 };
-            wallRoad[32] = new int[] { 8, 9 };
-            // Portes
-            wallRoad[33] = new int[] { 5, 0 };
-            wallRoad[34] = new int[] { 5, 10 };
-        }
-    }
 }
--- Rogue-Roan/Models/Mapping/RoomRestriction.cs
+++ Rogue-Roan/Models/Mapping/RoomRestriction.cs
@@ -15,26 +15,27 @@ namespace Rogue_Roan.Models.Mapping
         /// <param name="crossRoad">est un tableau de coordonées</param>
 
         public int[][] crossRoad = new int[RoomDecoration.roomWidth+RoomDecoration.roomHeight-4][];
-        int count = 0;
-        public void CrossRoadAssignation()
+        int countCross = 0;
+        public int[][] CrossRoadAssignation()
         {
             // Ligne vertical. i commence à 1 car les position en 0 concerne les portes qui me seront fournie
             for (int i = 1; i < RoomDecoration.roomWidth; i++)
             {
-                crossRoad[count] = new int[] { i, RoomDecoration.roomWidth / 2 };
-                count++;
+                crossRoad[countCross] = new int[] { i, RoomDecoration.roomWidth / 2 };
+                countCross++;
             }
             //Ligne horizontale. i commence à 1 car les position en 0 concerne les portes qui me seront fournie
             for (int i = 1; i < RoomDecoration.roomHeight; i++)
             {
-                crossRoad[count] = new int[] { i, RoomDecoration.roomHeight / 2 };
-                count++;
+                crossRoad[countCross] = new int[] { i, RoomDecoration.roomHeight / 2 };
+                countCross++;
             }
             // Affichage des coordonnées
             foreach (var position in crossRoad)
             {
                 Console.WriteLine($"Position: ({position[0]}, {position[1]})");
             }
+            return crossRoad;
         }
 
         /// <summary>
@@ -42,14 +43,31 @@ namespace Rogue_Roan.Models.Mapping
         /// </summary>
         /// <param name="wallRoad">est un tableau de coordonées</param>
 
+        int countWall = 0;
         public int[][] wallRoad = new int[(RoomDecoration.roomWidth-2)*2 + (RoomDecoration.roomHeight-2) * 2][];
-        public void WallRoadAssignation()
+        public int[][] WallRoadAssignation()
         {
             // Long du mur nord
             for (int i = 1; i < RoomDecoration.roomWidth; i++)
             {
-                wallRoad[i]
+                wallRoad[countWall] = new int[] { 1, i }; //Remplacer le 1 par une formule?
             }
+            // Long du mur sud
+            for (int i = 1; i < RoomDecoration.roomWidth; i++)
+            {
+                wallRoad[countWall] = new int[] { RoomDecoration.roomWidth-1, i };
+            }
+            // Long du mur ouest
+            for (int i = 2; i < RoomDecoration.roomHeight-2; i++)
+            {
+                wallRoad[countWall] = new int[] { i, 1 };
+            }
+            // Long du mur est
+            for (int i = 2; i < RoomDecoration.roomHeight-2; i++)
+            {
+                wallRoad[countWall] = new int[] { i, RoomDecoration.roomWidth-1 };
+            }
+            return wallRoad;
         }
 
     }
